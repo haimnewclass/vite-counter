1c47707d386cb64b7ff463fbe1549eb8c6ddb11b
haimnewclass <haim@newclass.co.il>
2025-04-06 16:11:30 +0300

    fix: correct text formatting in HTML and TypeScript files

--- src/main.ts
+++ src/main.ts
@@ -16,9 +16,9 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
       <button id="counter" type="button"></button>
     </div>
     <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
+      Click on the Vite and TypeScript logos to learn more 1
     </p>
-  </div>
+  </div>  
 `
 
 setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
